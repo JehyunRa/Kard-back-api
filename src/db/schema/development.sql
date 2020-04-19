5d71bcc34984de79f5a6956ebb57fe0eb030dd21
JehyunRa <jra4862@gmail.com>
2020-04-19 14:17:55 -0400

    a

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (user_id, categories, favorites, name, email, age, gender)
 VALUES
-    ('aaa@gmail.com', '{ "album":{}, "book":{}, "movie":{}, "resto":{} }', '{ "fav":"album book movie resto" }', 'aaa', 'aaa@gmail.com', 20, 'male'),
-    ('bbb@gmail.com', '{ "album":{}, "book":{}, "movie":{}, "resto":{} }', '{ "fav":"album book movie resto" }', 'bbb', 'bbb@gmail.com', 30, 'female');
+    ('aaa', '{ "album":{}, "book":{}, "movie":{}, "resto":{} }', '{ "fav":"album book movie resto" }', 'aaa', 'aaa@gmail.com', 20, 'male'),
+    ('bbb', '{ "album":{}, "book":{}, "movie":{}, "resto":{} }', '{ "fav":"album book movie resto" }', 'bbb', 'bbb@gmail.com', 30, 'female');
